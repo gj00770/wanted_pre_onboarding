8331517a6c695773543297d3f300c24d6832807d
gj00770 <gj00770@georgiasouthern.edu>
2022-04-17 19:26:00 +0900

    fix readme

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -21,14 +21,14 @@ function Tab() {
     return (
       <div className='Tab'   >
           <div className='tab_menu' >
-                <div className='tab_menu_food' onClick={(e)=>clickBtn(e)} >감자</div>
-                <div className='tab_menu_food' onClick={(e)=>clickBtn(e)} >고구마</div>
-                <div className='tab_menu_food' onClick={(e)=>clickBtn(e)} >카레라이스</div>
+                <div className='tab_menu_food' onClick={(e)=>clickBtn(e)}  style={{color: current==='감자'? 'black' :"#D3D3D3"}}>감자</div>
+                <div className='tab_menu_food' onClick={(e)=>clickBtn(e)} style={{color: current==='고구마'? 'black' :"#D3D3D3"}}>고구마</div>
+                <div className='tab_menu_food' onClick={(e)=>clickBtn(e)} style={{color: current==='카레라이스'? 'black' :"#D3D3D3"}}>카레라이스</div>
           </div>
           <div className='tab_line'>
               <div className='tab_line_level'  
-              style={{transform:current === "감자" ? "translateX(0%)": 
-              current == "고구마"? 'translateX(100%)':
+               style={{transform:current === "감자" ? "translateX(0%)": 
+               current == "고구마"? 'translateX(100%)':
                current=='카레라이스'? 'translateX(200%)':null }} >
               </div>
           </div>
